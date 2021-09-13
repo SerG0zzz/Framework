d30c1c4ac4acce0a6a62bc2e8d47c9339d8f75f5
SerG0zzz <SeroEgor@Gmail.com>
2021-09-13 15:55:42 +0300

    0.0.2.5
    Добавил тесты MiddlewareResolverTest.php и поправил Pipeline.php

---
+++ tests/Framework/Http/Pipeline/MiddlewareResolverTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace Framework\Http\Pipeline;
+
+use Laminas\Diactoros\Response;
+use Laminas\Diactoros\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class MiddlewareResolverTest extends TestCase
+{
+    /**
+     * @dataProvider getValidHandlers
+     * $param $handler
+     */
+    public function testDirect($handler)
+    {
+        $resolver = new MiddlewareResolver();
+        $middleware = $resolver->resolve($handler);
+
+        /** @var ResponseInterface $response */
+        $response = $middleware(
+            (new ServerRequest())->withAttribute('attribute', $value = 'value'),
+            new Response(),
+            new NotFoundMiddleware()
+        );
+
+        self::assertEquals([$value], $response->getHeader('X-Header'));
+    }
+
+    /**
+     * @dataProvider getValidHandlers
+     * @param $handler
+     */
+    public function testNext($handler)
+    {
+        //$resolver = new MiddlewareResolver();
+        $middleware = (new MiddlewareResolver())->resolve($handler);
+
+        /** @var ResponseInterface $response */
+        $response = $middleware(
+            (new ServerRequest())->withAttribute('next', true),
+            new Response(),
+            new NotFoundMiddleware()
+        );
+
+        self::assertEquals(404, $response->getStatusCode());
+    }
+
+    public function getValidHandlers()
+    {
+        return [
+            'Callable Callback' => [function (ServerRequestInterface $request, callable $next) {
+                if ($request->getAttribute('next')) {
+                    return $next($request);
+                }
+                return (new Response\HtmlResponse(''))
+                    ->withHeader('X-Header', $request->getAttribute('attribute'));
+            }],
+            'Callable Class' => [CallableMiddleware::class],
+            'Callable Object' => [new CallableMiddleware()],
+            'DoublePass Callback' => [function (ServerRequestInterface $request, ResponseInterface $response, callable $next) {
+                if ($request->getAttribute('next')) {
+                    return $next($request);
+                }
+                return $response
+                    ->withHeader('X-Header', $request->getAttribute('attribute'));
+            }],
+            'DoublePass Class' => [DoublePassMiddleware::class],
+            'DoublePass Object' => [new DoublePassMiddleware()],
+            'Interop Class' => [InteropMiddleware::class],
+            'Interop Object' => [new InteropMiddleware()],
+        ];
+    }
+
+    public function testArray()
+    {
+        $resolver = new MiddlewareResolver();
+
+        $middleware = $resolver->resolve([
+            new DummyMiddleware(),
+            new CallableMiddleware()
+        ]);
+
+        /** @var ResponseInterface $response */
+        $response = $middleware(
+            (new ServerRequest())->withAttribute('attribute', $value = 'value'),
+            new Response(),
+            new NotFoundMiddleware()
+        );
+
+        self::assertEquals(['dummy'], $response->getHeader('X-Dummy'));
+        self::assertEquals([$value], $response->getHeader('X-Header'));
+    }
+}
+
+class CallableMiddleware
+{
+    public function __invoke(ServerRequestInterface $request, callable $next): ResponseInterface
+    {
+        if ($request->getAttribute('next')) {
+            return $next($request);
+        }
+        return (new Response\HtmlResponse(''))
+            ->withHeader('X-Header', $request->getAttribute('attribute'));
+    }
+}
+
+class DoublePassMiddleware
+{
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        if ($request->getAttribute('next')) {
+            return $next($request);
+        }
+        return $response
+            ->withHeader('X-Header', $request->getAttribute('attribute'));
+    }
+}
+
+class InteropMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if ($request->getAttribute('next')) {
+            return $handler->handle($request);
+        }
+        return (new Response\HtmlResponse(''))
+            ->withHeader('X-Header', $request->getAttribute('attribute'));
+    }
+}
+
+class NotFoundMiddleware
+{
+    public function __invoke(ServerRequestInterface $request)
+    {
+        return new Response\EmptyResponse(404);
+    }
+}
+
+class DummyMiddleware
+{
+    public function __invoke(ServerRequestInterface $request, callable $next): ResponseInterface
+    {
+        return $next($request)
+            ->withHeader('X-Dummy', 'dummy');
+    }
+}
--- tests/Framework/Http/Pipeline/PipelineTest.php
+++ tests/Framework/Http/Pipeline/PipelineTest.php
@@ -3,9 +3,11 @@
 namespace Framework\Http\Pipeline;
 
 use Framework\Http\Pipeline\Pipeline;
+use Laminas\Diactoros\Response;
 use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\ServerRequest;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class PipelineTest extends TestCase
@@ -17,7 +19,7 @@ class PipelineTest extends TestCase
         $pipeline->pipe(new Middleware1());
         $pipeline->pipe(new Middleware2());
 
-        $response = $pipeline(new ServerRequest(), new Last());
+        $response = $pipeline(new ServerRequest(), new Response(), new Last());
 
         $this->assertJsonStringEqualsJsonString(
             json_encode(['middleware-1' => 1, 'middleware-2' => 2]),
@@ -28,7 +30,7 @@ class PipelineTest extends TestCase
 
 class Middleware1
 {
-    public function __invoke(ServerRequestInterface $request, callable $next)
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return $next($request->withAttribute('middleware-1', 1));
     }
@@ -36,7 +38,7 @@ class Middleware1
 
 class Middleware2
 {
-    public function __invoke(ServerRequestInterface $request, callable $next)
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return $next($request->withAttribute('middleware-2', 2));
     }
